7285e6ecb6a8df8cef40a91cf7705951da7e7393
Jose Ernesto Carreno Bueno <josernesto1989@gmail.com>
2021-12-28 10:50:08 -0500

    Adicionando recursos para heroku

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const users:Object|any={};
 let precioDollar:number=71;
 
 
-const fs = require('fs');
+// const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
